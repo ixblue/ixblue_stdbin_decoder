0fa6a824d8761fff5c78babb02ddfaae4219e2a2
Romain Reignier <rre@robopec.com>
2020-09-21 18:31:15 +0200

    Add ins algorithm status and ins system status bits description in emums

--- include/ixblue_stdbin_decoder/data_models/navigation_data/ins_algorithm_status.h
+++ include/ixblue_stdbin_decoder/data_models/navigation_data/ins_algorithm_status.h
@@ -8,10 +8,148 @@ namespace Data
 
 struct INSAlgorithmStatus
 {
+    enum Status1
+    {
+        NAVIGATION,
+        ALIGNMENT,
+        FINE_ALIGNMENT,
+        DEAD_RECKONING,
+        GPS_ALTITUDE,
+        DEPTHSENSOR_ALTITUDE,
+        ZERO_ALTITUDE,
+        HYDRO_ALTITUDE,
+        LOG_RECEIVED,
+        LOG_VALID,
+        LOG_WAITING,
+        LOG_REJECTED,
+        GPS_RECEIVED,
+        GPS_VALID,
+        GPS_WAITING,
+        GPS_REJECTED,
+        USBL_RECEIVED,
+        USBL_VALID,
+        USBL_REJECTED,
+        DEPTH_RECEIVED,
+        DEPTH_VALID,
+        DEPTH_WAITING,
+        DEPTH_REJECTED,
+        LBL_RECEIVED,
+        LBL_VALID,
+        LBL_WAITING,
+        LBL_REJECTED,
+        ALTITUDE_SATURATION,
+        SPEED_SATURATION,
+        INTERPOLATION_MISSED,
+        HEAVE_INITIALISATION
+    };
+
+    enum Status2
+    {
+        WATERTRACK_RECEIVED,
+        WATERTRACK_VALID,
+        WATERTRACK_WAITING,
+        WATERTRACK_REJECTED,
+        GPS2_RECEIVED,
+        GPS2_VALID,
+        GPS2_WAITING,
+        GPS2_REJECTED,
+        // 8-11 reserved
+        ALTITUDE_RECEIVED = 12,
+        ALTITUDE_VALID,
+        ALTITUDE_WAITING,
+        ALTITUDE_REJECTED,
+        ZUPT_ACTIVATED,
+        ZUPT_VALID,
+        RO_ZUPT_ACTIVATED,
+        RO_ZUPT_VALID,
+        // 20-21 reserved
+        FAST_ALIGNEMENT = 22,
+        EMULATION_MODE,
+        EMLOG_RECEIVED,
+        EMLOG_VALID,
+        EMLOG_WAITING,
+        EMLOG_REJECTED,
+        MANUALGPS_RECEIVED,
+        MANUALGPS_VALID,
+        MANUALGPS_WAITING,
+        MANUALGPS_REJECTED
+    };
+
+    enum Status3
+    {
+        SVL_RECEIVED,
+        SVL_VALID,
+        SVL_WAITING,
+        SVL_REJECTED,
+        EMLOG2_RECEIVED,
+        EMLOG2_VALID,
+        EMLOG2_WAITING,
+        EMLOG2_REJECTED,
+        USBL2_RECEIVED,
+        USBL2_VALID,
+        USBL2_WAITING,
+        USBL2_REJECTED,
+        USBL3_RECEIVED,
+        USBL3_VALID,
+        USBL3_WAITING,
+        USBL3_REJECTED,
+        // 16 reserved
+        CALCHK = 17,
+        RESTORE_ATTITUDE_FAILED,
+        REL_SPD_ZUPT_ACTIVATED,
+        REL_SPD_ZUPT_VALID,
+        EXT_SENSOR_OUTDATED,
+        SENSOR_USED_BEFORE_CALIB,
+        RESTORE_ATTITUDE_REJECTED,
+        // 24-26 reserved
+        POLAR_VALIDITY = 27,
+        FIRM_INCOMPATIBLES,
+        IMO_ALIGNMENT,
+        OPTIMAL_ALIGNMENT,
+        STRATEGIC_ALIGNMENT,
+    };
+
+    enum Status4
+    {
+        LOG2_RECEIVED,
+        LOG2_VALID,
+        LOG2_WAITING,
+        LOG2_REJECTED,
+        WATERTRACK2_RECEIVED,
+        WATERTRACK2_VALID,
+        WATERTRACK2_WAITING,
+        WATERTRACK2_REJECTED,
+        DVL_DIST_TRAVELLED_VALID,
+        DVL_CALIBRATION_NONE,
+        DVL_ROUGH_CALIBRATION,
+        DVL_FINE_CALIBRATION,
+        DVL_CHECK_CALIBRATION,
+        MANUAL_ATT_RECEIVED,
+        MANUAL_ATT_VALID,
+        MANUAL_ATT_WAITING,
+        MANUAL_ATT_REJECTED,
+        FIXED_HEADING,
+        DEGRADED_MODE,
+        VTG_RECEIVED,
+        VTG_VALID,
+        VTG_WAITING,
+        VTG_REJECTED,
+        VTG2_RECEIVED,
+        VTG2_VALID,
+        VTG2_WAITING,
+        VTG2_REJECTED,
+        ALIX_SYST_SATURATION,
+        CINT_FLASH_MEMORY_WRERR,
+        CINT_FLASH_MEMORY_ERERR,
+        ODO_CALIBRATION_DONE,
+        INCONSISTENT_SENSOR_DATA
+    };
+
     uint32_t status1;
     uint32_t status2;
     uint32_t status3;
     uint32_t status4;
 };
+
 } // namespace Data
 } // namespace ixblue_stdbin_decoder
